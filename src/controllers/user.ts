ac69f8f7234c8e55551221cff7bc85481c54e02d
itforget CORDEIRO <itforget11@gmail.com>
2024-05-22 19:17:48 +0000

    fix: auth

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,40 +1,40 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 async function loginUser(req: Request, res: Response) {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res
-        .status(422)
-        .json({ error: "Please provide all the required fields" });
-    }
-  
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-  
-    const isMatch = await bcrypt.compare(password, user.password);
-  
-    if (!isMatch) {
-      return res.status(422).json({ error: "Invalid password" });
-    }
-  
-    try {
-      const token = jwt.sign({ id: user._id }, process.env.SECRET, {
-        expiresIn: "1h",
-      });
-      res
-        .status(200)
-        .json({ msg: "User logged in successfully", token, userId: user._id });
-    } catch (error: any) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    }
+  if (!email || !password) {
+    return res
+      .status(422)
+      .json({ error: "Please provide all the required fields" });
   }
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(422).json({ error: "Invalid password" });
+  }
+
+  try {
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      expiresIn: "1h",
+    });
+    res
+      .status(200)
+      .json({ msg: "User logged in successfully", token, userId: user._id });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+}
 
 async function registerUser(req: Request, res: Response) {
   const { name, email, password, confirmPassword, date } = req.body;
@@ -61,7 +61,7 @@ async function registerUser(req: Request, res: Response) {
     email,
     date,
     password: passwordHash,
-    confirmPassword: passwordHash
+    confirmPassword: passwordHash,
   });
 
   try {
@@ -73,4 +73,34 @@ async function registerUser(req: Request, res: Response) {
   }
 }
 
-export { loginUser, registerUser }
+async function authUser(req: Request, res: Response) {
+  const id = req.params.id;
+  if (!id) {
+    return res
+      .status(422)
+      .json({ error: "Please provide all the required fields" });
+  }
+  const user = await User.findById(id, "-password -confirmPassword");
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  res.status(200).json({ user });
+}
+
+function checkToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ error: "Invalid token" });
+  }
+}
+
+export { loginUser, registerUser, checkToken, authUser };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { loginUser, registerUser } from '../controllers/user';
+import { loginUser, registerUser, checkToken, authUser } from '../controllers/user';
 const router = Router()
 
+router.get("/auth/:id", checkToken, authUser)
 router.post('/auth/login', loginUser)
 router.post("/auth/register", registerUser);
 
